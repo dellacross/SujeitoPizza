13567abbb749cf0c990dd34c6a453b038338c644
dellacross <victordella652@gmail.com>
2024-07-17 13:27:48 -0300

    habilitar requisicao por qualquer ip (cors) + tratamente (basico) de erro

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Response, Request } from 'express'
 const router = Router()
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ok: true})
+    return res.json({nome: "Sujeito Pizza"})
 })
 
 export { router }
