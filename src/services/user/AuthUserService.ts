c32289b534872d9af5df13b0d877c4dbe08d83da
dellacross <victordella652@gmail.com>
2024-07-20 12:19:10 -0300

    gerar token ao fazer login

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,45 @@
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs"; // compara senha passada com senha criptografada
+import { sign } from 'jsonwebtoken'
+
+interface AuthRequest {
+    email: string,
+    password: string
+}
+
+class AuthUserService {
+    async execute({email, password}: AuthRequest) {
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if(!user) throw new Error('Usuário ou senha incorreto!')
+
+        const passwordMath = await compare(password, user.password)
+
+        if(!passwordMath) throw new Error('Usuário ou senha incorreto!')
+
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }   
+        )
+
+        return {
+            id: user.id,
+            name: user.name, 
+            email: user.email,
+            token: token
+        }
+    }
+}
+
+export{ AuthUserService }
