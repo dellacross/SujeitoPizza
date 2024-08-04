509c57465db21ebcc768b3ffdfa2706e2120f882
dellacross <victordella652@gmail.com>
2024-08-04 19:03:43 -0300

    new routes - createCategory e listCategories

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,19 @@ import { Router } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router()
 
+// user routes
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// categories routes
+router.post('/category', new CreateCategoryController().handle)
+router.get('/category', new ListCategoryController().handle)
 
 export { router }
